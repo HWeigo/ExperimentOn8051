95669342a560f95461fa2541bdc5ce4a63182738
HWeigo <746144870@qq.com>
2019-04-18 19:31:22 +0800

    切换功能修复

--- Experiment3/experiment_3forReport.asm
+++ Experiment3/experiment_3forReport.asm
@@ -3,71 +3,63 @@
 			MINUTE_LOW EQU 32H
 			MINUTE_HIGH EQU 33H
 			TCOUNT EQU 34H
-			
-			ORG 0H
-			AJMP MAIN
-			ORG 0003H
-			AJMP INTF
+						
+  		    ORG 0H
+			AJMP MAIN		
 			ORG 000BH
 			AJMP TINT0
-			ORG 0013H
-			AJMP INTS
 
-			ORG 60H
+			ORG  60H
+MAIN:		
+			;初始化
+			MOV	30H,#0H
+			MOV 31H,#0H
+			MOV	32H,#0H
+			MOV 33H,#0H
+			MOV 34H,#0H
+			MOV FLAG,#0H
 
-MAIN:		;初始化
-			MOV SECOND_LOW,#0H
-			MOV SECOND_HIGH,#0H
-			MOV MINUTE_LOW,#0H
-			MOV MINUTE_HIGH,#0H
-			MOV TCOUNT,#0H
-			MOV R1,#0H 		;R1作为暂停用flag
-			
-			SETB EA			;CPU允许中断
-			
-			;外部中断初始化
-			CLR IT0			;设置外部中断触发方式
-			CLR IT1
-			SETB PX0		;设置INT0高优先级
-			CLR PX1			;设置INT1高优先级
-			SETB EX0		;开放中断
-			SETB EX1
-		
-
-			;定时器中断初始化
-			MOV TMOD,#01H 	;t0使用模式1定时器
-			MOV TH0,#4CH	;设置定时50ms
+			;定时器中断使能
+			MOV TMOD,#01H 		;t0使用模式1定时器
+			MOV TH0,#04CH	 		;设置定时50ms
 			MOV TL0,#0H
-			SETB ET0		;t1允许中断						 
-			SETB TR0 		;启动定时						 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;			
-
-LOOP:	   	AJMP DISPLAY
-			SJMP LOOP
-
+			SETB ET0		 	;t1允许中断
+			SETB TR0 			;启动定时
+			
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;外部中断INT0用于切换R1（状态机，用作暂停flag）
-;R1-0运行，R1-1暂停
-			ORG 0110H
-INTF:		INC R1
-			SETB ET0		
-			CJNE R1,#2H,NEXT_INT
-			MOV R1,#0H
-NEXT_INT:
-			CLR ET0
-			RETI
+;main函数			
+LOOP:	  	;按键检测
+			JNB P1.0,CLEAR
+			JNB P1.1,STOP
+			JNB P1.2,START
+			ACALL DISPLAY
+
+			SJMP LOOP			
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;外部中断INT1用于清零
-INTS:		MOV SECOND_LOW,#0H	  ;重置所有寄存器
+;清零
+CLEAR:			 		
+			MOV	SECOND_LOW,#0H
 			MOV SECOND_HIGH,#0H
-			MOV MINUTE_LOW,#0H
+			MOV	MINUTE_LOW,#0H
 			MOV MINUTE_HIGH,#0H
 			MOV TCOUNT,#0H
-			
-			RETI
+			LJMP LOOP
+			RET
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;定时器中断
-TINT0:		MOV TH0,#4CH	 	;重置初值
+;暂停与继续
+STOP:		CLR ET0
+			LJMP LOOP
+			;RET
+
+START:		SETB ET0
+			LJMP LOOP
+			;RET
+
+			
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;定时INC寄存器
+			
+TINT0:		MOV TH0,#04CH	 	;重置初值
 			MOV TL0,#0H
 
 			INC TCOUNT
@@ -90,46 +82,43 @@ TINT0:		MOV TH0,#4CH	 	;重置初值
 			MOV A,MINUTE_HIGH
 			CJNE A,#6,NEXT_TINT	;分十位未到6，跳出
 			MOV MINUTE_HIGH,#0H		;重置分个位
-
-NEXT_TINT:
+						
+NEXT_TINT:			
 			RETI
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;数码管显示
+			
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;			
 DISPLAY:	;第一个数码管
 			MOV	DPTR,#DSEG1
-			MOV  A,MINUTE_HIGH
+			MOV  A,30H
 			MOVC	A,@A+DPTR
-			MOV	DPTR,#7FF0H			     
+			MOV	DPTR,#7FF0H		     
 			MOVX	@DPTR,A
-			
 			ACALL DELAY_SHORT
 
 			;第二个数码管
 			MOV	DPTR,#DSEG1
-			MOV A,MINUTE_LOW
+			MOV A,31H
 			MOVC	A,@A+DPTR
 			MOV	DPTR,#7FF1H			     
 			MOVX	@DPTR,A
-
 			ACALL DELAY_SHORT
 
 			;第三个数码管
 			MOV	DPTR,#DSEG1
-			MOV A,SECOND_HIGH
+			MOV A,32H
 			MOVC	A,@A+DPTR
+			CLR ACC.7				 	;点亮小数点位
 			MOV	DPTR,#7FF2H			     
 			MOVX	@DPTR,A
-
 			ACALL DELAY_SHORT
 
 			;第四个数码管
 			MOV	DPTR,#DSEG1
-			MOV A,SECOND_LOW
+			MOV A,33H
 			MOVC	A,@A+DPTR
 			MOV	DPTR,#7FF3H			     
 			MOVX	@DPTR,A
-
 			ACALL DELAY_SHORT
 
 			RET
@@ -142,7 +131,7 @@ DEL_SHORT: MOV R6,#250
            RET
 
 DELAY_LONG:                   
-           MOV R7,#100
+           MOV R7,#250
 DEL_LONG:  MOV R6,#250
            DJNZ R6,$
            DJNZ R7,DEL_LONG
